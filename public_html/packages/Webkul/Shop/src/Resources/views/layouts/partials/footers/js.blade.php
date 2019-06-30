8ab9ebed448dae8beb039b9f0d10693f5ab7cd6a
trunghieu321 <nguyenhieu.12a4@gmail.com>
2019-06-30 16:06:04 +0700

    bagisto-1: init

---
+++ public_html/packages/Webkul/Shop/src/Resources/views/layouts/partials/footers/js.blade.php
@@ -0,0 +1,29 @@
+<!-- Modernizer js -->
+<script src="{{asset("themes/truemart/assets")}}/js/vendor/modernizr-3.5.0.min.js"></script>
+<!-- jquery 3.2.1 -->
+<script src="{{asset("themes/truemart/assets")}}/js/vendor/jquery-3.2.1.min.js"></script>
+<!-- Countdown js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery.countdown.min.js"></script>
+<!-- Mobile menu js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery.meanmenu.min.js"></script>
+<!-- ScrollUp js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery.scrollUp.js"></script>
+<!-- Nivo slider js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery.nivo.slider.js"></script>
+<!-- Fancybox js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery.fancybox.min.js"></script>
+<!-- Jquery nice select js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery.nice-select.min.js"></script>
+<!-- Jquery ui price slider js -->
+<script src="{{asset("themes/truemart/assets")}}/js/jquery-ui.min.js"></script>
+<!-- Owl carousel -->
+<script src="{{asset("themes/truemart/assets")}}/js/owl.carousel.min.js"></script>
+<!-- Bootstrap popper js -->
+<script src="{{asset("themes/truemart/assets")}}/js/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="{{asset("themes/truemart/assets")}}/js/bootstrap.min.js"></script>
+<!-- Plugin js -->
+<script src="{{asset("themes/truemart/assets")}}/js/plugins.js"></script>
+<!-- Main activaion js -->
+<script src="{{asset("themes/truemart/assets")}}/js/main.js"></script>
+<script src="{{asset("themes/truemart/assets")}}/js/app.js"></script>
